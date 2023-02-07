7a9512063115c0a61bc930c5a5af632d898088dc
Nikita Lukashevich <nick939317@gmail.com>
2023-02-07 21:40:53 +0300

    greated lesson 5

---
+++ lessson 5/script.js
@@ -0,0 +1,16 @@
+// Задание 1
+
+// Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+// Вам понадобятся методы строк.
+
+// let ucFirst = (str) => str[0].toLocaleUpperCase() + str.substring(1);
+
+// console.log(ucFirst('myfreedom'));
+
+// Задание 2
+
+// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
+// Функция должна быть нечувствительна к регистру.
+
+// let checkSpam = (str) 
